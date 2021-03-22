d38dd10601158385508d00033bd4a0a3de62e081
denismikhno <denismikhno@gmail.com>
2021-03-22 20:52:35 +0300

    my first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let aVar = 'str\'';
+let avar = 50;
+var b = 25;
+
+//let str = eval('2 + 2 - 2');
+//let str1 = '2' === 2;
+
+
+//let data = parseInt(prompt('Введи число'));
+
+/*if (data == 25) {
+    alert('ok');
+} else if (data == 30) {
+    alert('if');
+} else {
+    alert('all');
+}*/
+
+// for (let i = 1; i <= data; i++) {
+//     if (i == 5) {
+//         continue;
+//     }
+//     //document.write('num => ' + i  + '<br>');
+// }
+
+
+try {
+    alert('start');
+    dfgdgf;
+} catch (e) {
+    document.write('name => ' + e.name + 'mess => ' + e.message + ' stack => ' + e.stack);
+}
+
+
+//alert('end block');
+//console.log(i);
+// switch (data) {
+//     case 25:
+//         alert('ok');
+//         break;
+//     case 30:
+//         alert('if');
+//         break;
+//     default:
+//         alert('all')
+// }
+
+
+//let res = data > 20 ? 'ОК' : 'NOT OK';
+
+
+//document.write(res);
+
+/*
+document.write(" <h1> Hello World! </h1> ");
+
+console.log(a);
+
+*/
