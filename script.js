52c7610daf47cace2a58d4ab14b1b7bfa52f5273
denomy <denomy@yandex.ru>
2021-04-22 20:38:05 +0300

    P-22.04

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
 
-let num = 50;
-
-let str = `АБВГДЕЁЖ ЗИЙКЛМНОПРС ТУФХЦЧШЩЪЫЬЭ ЮЯабвгдеёж зийклмнопрсту фхцчшщъыьэюя`;
-
-let str1 = str[1];
-str[1] = 'i';
-
-let str2 = 'Hello';
-// let str3 = 'hello';
-// let res = str2.localeCompare(str3);
-// let res1 = str2 === str3;
+let page  = {
 
 
-let numStr = num;
 
-let arr = str.split(' ');
-//let subStr = str.substring(0,15);
+    init: function () {
+        let div = document.createElement('div');
+        div.style.background = '#369';
+        div.className = 'div-1';
+
+        let textNode = document.createTextNode('hello world');
+
+        div.appendChild(textNode);
+        let comment = document.createComment('comment hello world');
+
+        let attr = document.createAttribute('name');
+        attr.value = 'vasya';
+        div.setAttributeNode(attr);
 
 
-// for (let i = 0; i<str.length; i++){
-//     document.write(`<div>code =>   ${str.charCodeAt(i)}</div><br>`);
-// }
+        // document.body.appendChild(div);
+        // document.body.appendChild(div.cloneNode(true));
+        //document.body.appendChild(comment);
 
-console.log(arr);
+       // document.body.insertBefore(comment, document.body.firstChild);
+      //  document.body.insertBefore(comment, document.body.children[10]);
+       // document.body.insertBefore(comment, document.body.children[1]);
+       // document.body.insertBefore(comment, undefined);
+
+        // let elem = document.body.children[1];
+        // elem.parentNode.insertBefore(comment, elem);
+
+        //comment.parentNode.removeChild(comment);
+      //  comment.remove();
+       // document.body.append(div);
+       // document.body.prepend(textNode);
+       // document.body.replaceChild(comment, document.body.firstChild);
+
+       // div.insertAdjacentHTML("beforebegin", "<b>bold</b>");
+
+        let fragment = document.createDocumentFragment();
+
+        for (let i = 0; i < 500; i++){
+           fragment.appendChild(div.cloneNode(true));
+        }
+
+        document.body.appendChild(fragment);
 
 
-//alert(numStr);
+       // console.log(fragment);
+
+
+    }
+}
+
+window.onload = function () {
+  page.init();
+}
