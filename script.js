c12fc173dfb46e7e3ff5de65eb81687c9097459c
denomy <denomy@yandex.ru>
2021-04-22 15:31:38 +0300

    H-22.04

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
+let page = {
 
-let num = 50;
+    posLeft: 45,
+    posTop: 45,
 
-let str = `АБВГДЕЁЖ ЗИЙКЛМНОПРС ТУФХЦЧШЩЪЫЬЭ ЮЯабвгдеёж зийклмнопрсту фхцчшщъыьэюя`;
+    left: function () {
+        // минимальная граница слева 45px
+        if (this.posLeft > 45) {
+            this.posLeft -= 15;
+            this.moveBlock();
+        }
+    },
 
-let str1 = str[1];
-str[1] = 'i';
+    right: function () {
+        // двигаем пока не достигли правой границы окна браузера
+        if (this.posLeft < this.brauserWidth) {
+            this.posLeft += 15;
+            this.moveBlock();
+        }
 
-let str2 = 'Hello';
-// let str3 = 'hello';
-// let res = str2.localeCompare(str3);
-// let res1 = str2 === str3;
+    },
 
+    top: function () {
+        // инимальная граница сверху 45px
+        if (this.posTop > 45) {
+            this.posTop -= 15;
+            this.moveBlock();
+        }
+    },
 
-let numStr = num;
+    bottom: function () {
+        // двигаем пока не достигли нижней границы окна браузера
+        if (this.posTop < this.brauserHeight) {
+            this.posTop += 15;
+            this.moveBlock();
+        }
+    },
 
-let arr = str.split(' ');
-//let subStr = str.substring(0,15);
+    moveBlock: function () {
+        this.block.style.left =  this.posLeft + "px";
+        this.block.style.top = this.posTop + "px";
+        console.log(this.brauserWidth);
+        console.log(this.posLeft);
+    },
 
+    init: function () {
+        this.block = document.querySelector('.block');
+        // ширина окна браузера без полос прокрутки
+        this.brauserWidth = document.documentElement.clientWidth - 150;
+        // высота браузера без полос прокрутки
+        this.brauserHeight = document.documentElement.clientHeight - 150;
+    }
+}
 
-// for (let i = 0; i<str.length; i++){
-//     document.write(`<div>code =>   ${str.charCodeAt(i)}</div><br>`);
-// }
-
-console.log(arr);
-
-
-//alert(numStr);
+window.onload = function () {
+    page.init();
+}
