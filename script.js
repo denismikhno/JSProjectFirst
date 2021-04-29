884a1adc00aa1a0f3909f0809e7cff8b0dd11180
denomy <denomy@yandex.ru>
2021-04-29 22:44:25 +0300

    P-29.04

--- script.js
+++ script.js
@@ -1,28 +1,107 @@
 
-let num = 50;
 
-let str = `АБВГДЕЁЖ ЗИЙКЛМНОПРС ТУФХЦЧШЩЪЫЬЭ ЮЯабвгдеёж зийклмнопрсту фхцчшщъыьэюя`;
+function PhoneBookItem(name, phone, photo){
+    this.name = name;
+    this.phone = phone;
+    this.photo = photo;
 
-let str1 = str[1];
-str[1] = 'i';
+    this.toElement = function () {
+       let div = document.createElement('div');
+       let photo = document.createElement('div');
+       let right = document.createElement('div');
+       let name = document.createElement('div');
+       let phone = document.createElement('div');
 
-let str2 = 'Hello';
-// let str3 = 'hello';
-// let res = str2.localeCompare(str3);
-// let res1 = str2 === str3;
+       div.className = 'card';
+       photo.className = 'photo';
+       right.className = 'right';
+       name.className = 'name';
+       phone.className = 'phone';
+
+       name.appendChild(document.createTextNode(this.name));
+       phone.appendChild(document.createTextNode(this.phone));
+
+       div.style.position = 'relative';
+       div.style.paddingLeft = '50px';
+       div.style.minHeight = '50px';
+
+       photo.style.backgroundImage = `url(${this.photo})`;
+
+       div.appendChild(photo);
+       div.appendChild(right);
 
 
-let numStr = num;
-
-let arr = str.split(' ');
-//let subStr = str.substring(0,15);
+       right.appendChild(name);
+       right.appendChild(phone);
 
 
-// for (let i = 0; i<str.length; i++){
-//     document.write(`<div>code =>   ${str.charCodeAt(i)}</div><br>`);
-// }
+       return div;
 
-console.log(arr);
+    }
+
+}
 
 
-//alert(numStr);
+
+
+let page = {
+
+    phones: [],
+
+    addPhone: function () {
+        this.phones.push(new PhoneBookItem(
+              this.nameInput.value,
+              this.phoneInput.value,
+              this.photoInput.value.length > 0 ? this.photoInput.value : 'https://www.sightandsound.co.uk/wp-content/uploads/2020/05/minivision_homescreen_v2.jpg'
+            ));
+
+        this.redRaw();
+
+    },
+
+    redRaw: function () {
+
+        let _this = this;
+
+        this.phonebook.innerHTML = '';
+        this.phones.forEach(function (elem) {
+             let li = document.createElement('li');
+             li.appendChild(elem.toElement());
+             _this.phonebook.appendChild(li);
+        })
+    },
+
+    events: function() {
+        let _this = this;
+
+        this.add.addEventListener('click', function () {
+              this.addPhone();
+              console.log(this);
+        }.bind(this))
+
+
+
+
+
+    },
+
+    init: function () {
+
+       this.nameInput = document.querySelector('#name');
+       this.phoneInput = document.querySelector('#phone');
+       this.photoInput = document.querySelector('#photo');
+       this.phonebook = document.querySelector('.phone-book-list');
+       this.add = document.querySelector('.add');
+
+       this.events();
+    }
+
+};
+
+window.addEventListener('load', function () {
+    page.init();
+    //  document.addEventListener('click', function () {
+    //          alert('ok');
+    //  })
+
+});
